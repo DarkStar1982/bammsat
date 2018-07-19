fed076bb62015bc969844d19fe120e5baeddd4af
Denys <d.silin@cranfield.ac.uk>
2018-07-19 09:25:13 +0000

    Initial commit

---
+++ bammsat.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+import serial
+import time
+serialport = serial.Serial("/dev/ttyS0", 115200, timeout=0.5)
+
+while(True):
+	degrees = 26.5 
+	pascals = 101035
+	hectopascals = pascals / 100
+	humidity = 10
+
+	x = "%3.1f, %3.1f, %3.1f\r\n" % (degrees, hectopascals, humidity)
+	print (x)
+	print len(x)
+	serialport.write(x)
+	time.sleep(11);
