d8b992ebdf35973ab1d389aa4f8e3686e7a3f5dc
Denys <d.silin@cranfield.ac.uk>
2018-07-20 10:26:51 +0000

    Adding latest changes

--- bammsat.py
+++ bammsat.py
@@ -4,13 +4,13 @@ import time
 serialport = serial.Serial("/dev/ttyS0", 115200, timeout=0.5)
 
 while(True):
-	degrees = 26.5 
+	degrees = 24.3 
 	pascals = 101035
 	hectopascals = pascals / 100
 	humidity = 10
-
 	x = "%3.1f, %3.1f, %3.1f\r\n" % (degrees, hectopascals, humidity)
 	print (x)
-	print len(x)
 	serialport.write(x)
+	line = serialport.readline()
+	print line
 	time.sleep(11);
