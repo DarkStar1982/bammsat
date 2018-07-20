f04b3a574fb6f155ced10e5313d0aa9034b17917
Dennis <dennis@cranfield>
2018-07-20 11:25:32 +0100

    Adding the read from UART cide

--- bammsat.py
+++ bammsat.py
@@ -4,13 +4,14 @@ import time
 serialport = serial.Serial("/dev/ttyS0", 115200, timeout=0.5)
 
 while(True):
-	degrees = 26.3
+	degrees = 24.3
 	pascals = 101035
 	hectopascals = pascals / 100
 	humidity = 10
 
 	x = "%3.1f, %3.1f, %3.1f\r\n" % (degrees, hectopascals, humidity)
 	print (x)
-	print len(x)
 	serialport.write(x)
-	time.sleep(11);
+        line = serialport.readline()
+        print line
+        time.sleep(11);
