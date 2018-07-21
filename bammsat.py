76af0daa3839de1596461de983deb431463a7ab5
Dennis <dennis@cranfield>
2018-07-21 19:34:32 +0100

    Addign the inital architecture of subsystem simulator

--- bammsat.py
+++ bammsat.py
@@ -4,37 +4,77 @@ import time
 import struct
 serialport = serial.Serial("/dev/ttyS0", 115200, timeout=0.5)
 
-# simulate subsystem
-# read telemetry
-# simulate power levels
-def eps():
-    voltage = 5.0
-    power = 100    
+class Subsystem(object):
+    def __init__(self):
+        pass
 
-def send_test_packet():
-    packet = b"\x41\x42\x43\x45\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54"
-    eps_packet = [122,123,221,231,198,223]
+    def send_outbound_packet(self):
+        pass
+
+    def process_inbound_packet(packet_data):
+        pass
+
+# simulate power subsystem
+class EPS(Subsystem):
+    def __init__(self):
+        pass
     
-    values = [5.1, 100.0, 12.2, 12.3]
+    def send_outbound_packet(self):
+        packet = b"\x41\x42\x43\x45\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54"
+        eps_packet = [122,123,221,231,198,223]
+        values = [5.1, 100.0, 12.2, 12.3]
+        pack = bytearray()
+        pack.extend(b'\x01\x01')
+        pack.extend(b'\x01\x01')
+        for value in values:
+            ba = bytearray(struct.pack("f", value)) 
+            pack.extend(ba)
+        log_outbound_packet(pack)
+        return pack
 
-    pack = bytearray()
-    pack.extend(b'\x01\x30')
-    pack.extend(b'\x01\x01')
-    for value in values:
-        ba = bytearray(struct.pack("f", value)) 
-        pack.extend(ba)
-    return pack
+    def process_inbound_packet(self, packet_data):
+	log_inbound_packet(packet_data)
 
-while(True):
-	degrees = 24.3
-	pascals = 101035
-	hectopascals = pascals / 100
-	humidity = 10
-	#x = "%3.1f, %3.1f, %3.1f\r\n" % (degrees, hectopascals, humidity)
-	x = send_test_packet()
-        print(x)
-        print (len(x))
-	serialport.write(x)
-	line = serialport.readline()
-	print line
-	time.sleep(11);
+
+def create_subsystem(data):
+    subsystem = None
+    if data["subsystem"] == "eps":
+        subsystem = EPS()
+    return subsystem 
+
+def simulate_subsystem(data):
+    subsystem = create_subsystem(data)
+    while(True):
+        #generate outbound data
+	p_outbound = subsystem.send_outbound_packet()
+	
+        #exchange communications packets
+        serialport.write(p_outbound)
+	p_inbound = serialport.readline()
+        
+        #process inbound data
+        subsystem.process_inbound_packet(p_inbound)
+	time.sleep(data["packet_delay"]);
+
+def load_scenario():
+    scenario_data = {
+        "packet_delay": 11,
+        "subsystem": "eps"
+    }
+    return scenario_data
+
+def display_help():
+    pass
+
+
+def log_outbound_packet(data):
+    print data
+
+def log_inbound_packet(data):
+    print data
+
+def main():
+    scenario_data = load_scenario()
+    simulate_subsystem(scenario_data)
+
+main()
